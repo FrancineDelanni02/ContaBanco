e1a208b902a5ed28a46535a6aec939f54b105d5c
FrancineDelanni02 <delannifrancine@gmail.com>
2025-02-06 15:07:55 -0300

    mensagem

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o número da agência:");
+        int numero = sc.nextInt();
+
+        System.out.println("Digite sua agência: ");
+        String agencia = sc.next();
+
+        System.out.println("Digite o seu nome: ");
+        String nome = sc.next();
+
+        System.out.println("Digite o saldo da conta: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá "+ nome + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta " + numero + " e seu saldo "+ saldo+" já está disponível para saque");
     }
 }
